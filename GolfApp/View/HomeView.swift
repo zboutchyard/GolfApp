8f73bc33b1535ed3f30ad5696d1e0f143e9b2d0d
Zachary Boutchyard <zackboutchyard@gmail.com>
2023-11-23 19:29:03 -0500

    added postview to home screen, also added create post mock field.

--- GolfApp/View/HomeView.swift
+++ GolfApp/View/HomeView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var postSubmissionText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        NavigationStack {
+            ScrollView {
+                HStack {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .scaledToFill()
+                            .clipShape(Circle())
+                            .frame(width: 50, height: 50)
+                            .background {
+                                Circle().fill(Color("Gray"))
+                            }
+                        TextField("", text: $postSubmissionText, prompt: Text("write something..."))
+                    } .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding([.top, .leading, .bottom])
+                    HStack {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .padding(.trailing)
+                    }
+                   
+                }
+                .background(.white)
+                Spacer().frame(height: 13)
+                Divider()
+                Spacer().frame(height: 13)
+
+                VStack {
+                    PostView()
+                }
+            }
+            .frame(maxHeight: .infinity)
+            .background(Color("Gray"))
+        }
+       
     }
 }
 
