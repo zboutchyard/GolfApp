bb77f44e0a98a2b2a0b8c7491f6d0295e356f336
Zachary Boutchyard <zackboutchyard@gmail.com>
2023-12-16 14:47:29 -0500

    added return to login and sign out

---
+++ GolfApp/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GolfApp
+//
+//  Created by Zack Boutchyard on 12/16/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
